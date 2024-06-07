6a4fb09fd88f0f22bd1383e1b59190af24ebdac4
dutch <dutchisover@gmail.com>
2024-06-07 13:37:40 +0900

    GA追加

--- header.php
+++ header.php
@@ -2,6 +2,16 @@
 	<html lang="ja" prefix="og: http://ogp.me/ns#">
 
 	<head>
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-RJSZ3HTK7L"></script>
+		<script>
+		 window.dataLayer = window.dataLayer || [];
+		 function gtag(){dataLayer.push(arguments);}
+		 gtag('js', new Date());
+
+		 gtag('config', 'G-RJSZ3HTK7L');
+		</script>
+
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
